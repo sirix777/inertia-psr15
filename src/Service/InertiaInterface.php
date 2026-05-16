ed8473f3bababecd75f539930bd7b47381c8214a
sirix <sirix@inbox.lv>
2026-05-16 10:36:39 +0300

    feat: Add `location()` method to `InertiaInterface` for external URL redirection.

--- src/Service/InertiaInterface.php
+++ src/Service/InertiaInterface.php
@@ -4,18 +4,20 @@ declare(strict_types=1);
 
 namespace Sirix\InertiaPsr15\Service;
 
-use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ResponseInterface;
 
 interface InertiaInterface
 {
     /**
      * @param array<string, mixed> $props
      */
-    public function render(string $component, array $props = []): Response;
+    public function render(string $component, array $props = []): ResponseInterface;
 
     public function version(string $version): void;
 
     public function share(string $key, mixed $value = null): void;
 
     public function getVersion(): ?string;
+
+    public function location(ResponseInterface|string $destination, int $status = 302): ResponseInterface;
 }
